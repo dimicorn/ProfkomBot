d08adbe3b320350088734fc09acf25458817d6a8
dimicorn <zagorulia.ds@phystech.edu>
2021-11-08 22:23:08 +0300

    initial commit

---
+++ EmailSender.py
@@ -0,0 +1,29 @@
+import smtplib
+import ssl
+import json
+
+
+def send_email(receiver, key):
+    with open("config.json") as f:
+        config = json.load(f)
+    port = 587  # For starttls
+    smtp_server = "smtp.mail.ru"
+    sender_email = config["sender_email"]
+    receiver_email = receiver
+    password = config["sender_email_password"]
+    # FixMe: make that the email was in russian and add email subject
+    message = "\
+    Subject: confirmation code\n\n\nHello! Your code to confirm the mail of the trade union committee:\n" + str(key) +\
+              "\nSincerely, profkomBot "  # 🤖
+
+    context = ssl.create_default_context()
+    with smtplib.SMTP(smtp_server, port) as server:
+        server.ehlo()  # Can be omitted
+        server.starttls(context=context)
+        server.ehlo()  # Can be omitted
+        server.login(sender_email, password)
+        server.sendmail(sender_email, receiver_email, message)
+
+
+if __name__ == "__main__":
+    print("You're running the wrong file!")
---
+++ main.py
@@ -0,0 +1,262 @@
+import telebot
+from telebot import types
+import json
+import EmailSender
+import time
+
+# auth_status has values:
+# not auth - if user is not authorized
+# auth in process - if user has sent his email to the bot
+# code wait - if the bot has sent the confirmation code to user's email
+# auth - confirmation code is right, user is authorized
+auth_status = 'not auth'
+
+
+# random code generator
+def rand(x):
+    hour = x.tm_hour
+    mins = x.tm_min
+    sec = x.tm_sec
+    hour = hour * 3600
+    mins = mins * 60
+    t = hour + mins + sec
+    return t
+
+
+def main():
+    with open("config.json") as a, open("users.json") as b, \
+            open("email.json") as c:
+        config = json.load(a)
+        users = json.load(b)
+        emails = json.load(c)
+
+    codes = []
+
+    token = config["token"]
+    bot = telebot.TeleBot(token)
+
+    # defining /start command
+    @bot.message_handler(commands=['start'])
+    def welcome(message):
+        if message.from_user.id in users:  # user is authorized
+            bot.send_message(message.chat.id, "Я профкомБот🤖\nЖду ваших команд!")
+        else:  # user is not authorized/first time using the bot
+            markup = types.InlineKeyboardMarkup(row_width=2)
+            # inline buttons
+            item1 = types.InlineKeyboardButton("Да", callback_data='in labor union')
+            item2 = types.InlineKeyboardButton("Нет", callback_data='not in labor union')
+            markup.row(item1, item2)
+            bot.send_message(message.chat.id,
+                             "Здравствуйте! Я profkomBot 🤖\nСостоите ли вы в профсоюзе?", reply_markup=markup)
+
+    # defining /services command
+    @bot.message_handler(commands=['services'])
+    def service(message):
+        if auth_status == 'auth':  # only authorized users has access to the command
+            markup = types.InlineKeyboardMarkup(row_width=1)
+            # inline buttons
+            item1 = types.InlineKeyboardButton("Заявление на мат. помощь💰", callback_data='material help')
+            item2 = types.InlineKeyboardButton("Заявление на АПОС💸", callback_data='apos')
+            item3 = types.InlineKeyboardButton("Информация о профилактории🧖🏻‍♂️", callback_data='dispensary')
+            item4 = types.InlineKeyboardButton("Регистрация в программе «РЖД бонус»🚂", callback_data='train')
+            item5 = types.InlineKeyboardButton("Заявление на вступление в профсоюз📋", callback_data='labor union')
+            item6 = types.InlineKeyboardButton("Скидки и специальные предложения🤑", callback_data='discounts')
+            item7 = types.InlineKeyboardButton("Помощь🆘", callback_data='help')
+            items = [item1, item2, item3, item4, item5, item6, item7]
+            for i in items:
+                markup.row(i)
+            bot.send_message(message.chat.id, "Сервисы и услуги:", reply_markup=markup)
+        else:
+            bot.send_message(message.chat.id,
+                             "Простите, но я не знаю, что на это ответить...\n"
+                             "Воспользуйтесь одной из команд или кнопок")
+
+    '''
+    @bot.message_handler(commands=['write'])
+    def write(message):
+        bot.send_message(message.chat.id, "Привет!")
+        print(message.from_user.id, message.from_user.first_name,
+              message.from_user.last_name, message.from_user.username)
+    '''
+
+    # defining messages to the bot
+    @bot.message_handler(content_types=['text'])
+    def echo(message):
+        global auth_status
+        if auth_status == 'auth in process':
+            email = message.text
+            if email[-13:] == '@phystech.edu':  # checking if it's a phystech email
+                if email in emails:  # checking if it is in email.json
+                    users[message.from_user.id] = message.from_user.id
+                    auth_status = 'code wait'
+                    bot.send_message(message.chat.id,
+                                     "Я отправил на нее код с подтверждением. "
+                                     "Пожалуйста, отправьте мне этот код 📬")
+                    # generating confirmation code
+                    temp = time.gmtime()
+                    code = rand(temp)
+                    # sending confirmation code to the user
+                    EmailSender.send_email(email, code)
+                    codes.append(code)
+                    # writing the confirmation code in auth.json
+                    auth_file = open("auth.json", "w")
+                    json.dump(codes, auth_file)
+                    auth_file.close()
+
+                else:
+                    bot.send_message(message.chat.id, "Упс, вы не состоите в профсоюзе")
+                    auth_status = 'not auth'
+            else:
+                bot.send_message(message.chat.id, "Это не похоже на почту :O")
+        elif auth_status == 'code wait':
+            key = message.text
+            with open("auth.json") as f:
+                true_keys = json.load(f)
+            # FixMe: this should not work with many users
+            if key == str(true_keys[-1]):  # checking in the user texted the right confirmation code from the email
+                bot.send_message(message.chat.id, "Вы подтвердили вашу почту ✅")
+                auth_status = 'auth'
+                users[message.from_user.id] = message.from_user.id
+                users_file = open("users.json", "w")
+                json.dump(users, users_file)
+                users_file.close()
+            else:
+                bot.send_message(message.chat.id, "Код " + key + " неверный ❌\nНапишите код из письма")
+                auth_status = 'not auth'
+        else:
+            bot.send_message(message.chat.id,
+                             "Простите, но я не знаю, что на это ответить...\n"
+                             "Воспользуйтесь одной из команд или кнопок")
+
+    # buttons interaction
+    @bot.callback_query_handler(func=lambda call: True)
+    def callback_inline(call):
+        global auth_status
+        try:
+            if call.message:
+                bot.answer_callback_query(call.id, show_alert=False, text="🤖")
+                if call.data == 'in labor union':
+                    bot.send_message(call.message.chat.id, "Отправьте пожалуйста вашу физтеховскую почту 📩")
+                    auth_status = 'auth in process'
+                elif call.data == 'not in labor union':
+                    bot.send_message(call.message.chat.id,
+                                     "Для работы со мной необходимо быть челном профсоюза!"
+                                     "\nЗаполните и пришлите на нашу почту profkom@phystech.edu")
+                    doc = open('./Zayavlenie_priem_v_profsoyuz_student.pdf',
+                               'rb')
+                    bot.send_document(call.message.chat.id, doc)
+                if auth_status == 'auth':
+                    if call.data == 'material help':
+                        bot.send_message(call.message.chat.id,
+                                         "Ссылка на заявление на мат. помощь:\n https://vk.cc/av1z3P")
+                    elif call.data == 'apos':
+                        bot.send_message(call.message.chat.id,
+                                         "Заполните и пришлите на нашу почту\nprofkom@phystech.edu\n\n"
+                                         "Ссылка на заявление: https://vk.cc/c5v2DV")
+                        bot.send_message(call.message.chat.id,
+                                         "Или вы можете заполнить заявление в 224ГК\n\nВремя работы:\n"
+                                         "Понедельник-пятница 09:30-16:30")
+                    elif call.data == 'dispensary':
+                        bot.send_message(call.message.chat.id, "https://telegra.ph/FAQ-po-profilaktoriyu-08-30")
+                    elif call.data == 'train':
+                        bot.send_message(call.message.chat.id, "Ссылка на «РЖД бонус»:\nhttps://vk.cc/8FFtFa")
+                    elif call.data == 'labor union':
+                        doc = open('./Zayavlenie_priem_v_profsoyuz_student.pdf',
+                                   'rb')
+                        bot.send_document(call.message.chat.id, doc)
+                        bot.send_message(call.message.chat.id,
+                                         "Заполните и пришлите на нашу почту\nprofkom@phystech.edu")
+                    elif call.data == 'discounts':
+                        markup = types.InlineKeyboardMarkup(row_width=1)
+                        # inline buttons
+                        item1 = types.InlineKeyboardButton("Карта Metro", callback_data='metro')
+                        item2 = types.InlineKeyboardButton("РЖД бонус", callback_data='bonus')
+                        item3 = types.InlineKeyboardButton("Теле2", callback_data='tele2')
+                        item4 = types.InlineKeyboardButton("SkyEng", callback_data='skyeng')
+                        item5 = types.InlineKeyboardButton("Циферблат", callback_data='clock')
+                        item6 = types.InlineKeyboardButton("X-fit Studio", callback_data='x-fit')
+                        item7 = types.InlineKeyboardButton("кафе Boltay", callback_data='boltay')
+                        item8 = types.InlineKeyboardButton("кафе Теория", callback_data='theory')
+                        item9 = types.InlineKeyboardButton("Шницельная", callback_data='schnitzel')
+                        item10 = types.InlineKeyboardButton("Магазин Herb-store.ru", callback_data='herb-store')
+                        item11 = types.InlineKeyboardButton("Автошкола", callback_data='driving school')
+                        item12 = types.InlineKeyboardButton("Буфет тройки", callback_data='buffet')
+                        items = [item1, item2, item3, item4, item5, item6, item7, item8, item9, item10, item11, item12]
+                        for i in items:
+                            markup.row(i)
+                        bot.send_message(call.message.chat.id, "Компании партнёры:", reply_markup=markup)
+                    elif call.data == 'help':
+                        bot.send_message(call.message.chat.id,
+                                         "О возникших вопросах или проблемах пишите сюда:\n@maximperehod или @dimicorn")
+                    elif call.data == 'metro':
+                        bot.send_message(call.message.chat.id,
+                                         "Карта Metro\n\nОписание акции: Карта гостя работает 3 года.Наша карта: Срок "
+                                         "действия карты клиента METRO при условии хотя бы одной покупки в течение "
+                                         "12 месяцев неограничен. В случае отсутствия покупок в указанный период METRO "
+                                         "оставляет за собой право на ограничение доступа в свои торговые центры. "
+                                         "Также на ней копятся баллы и предоставляются скидки при оформлении\n\n"
+                                         "Как получить: https://vk.cc/8BhyW9")
+                    elif call.data == 'bonus':
+                        bot.send_message(call.message.chat.id,
+                                         "РЖД бонус\n\n"
+                                         "Описание акции: https://rzd-bonus.ru/about/student-program/\n\n"
+                                         "Как получить: https://vk.cc/8FFtFa")
+                    elif call.data == 'tele2':
+                        bot.send_message(call.message.chat.id,
+                                         "Теле2\n\nОписание акции: Выгодные тарифы\n\n"
+                                         "Как получить: https://www.mta-tele2.ru/")
+                    elif call.data == 'skyeng':
+                        bot.send_message(call.message.chat.id,
+                                         "SkyEng\n\nОписание акции: Выгодные тарифы\n\n"
+                                         "Как получить: https://corp.skyeng.ru/profkom-mfti")
+                    elif call.data == 'clock':
+                        bot.send_message(call.message.chat.id,
+                                         "Циферблат\n\nОписание акции:  (https://vk.com/clokface)\n"
+                                         "-20% с 10:00 до 19:00;\n- 300 руб. за ночь (с 00:00 до 8:00)\n\n"
+                                         "Как получить: Показать профсоюзный билет")
+                    elif call.data == 'x-fit':
+                        bot.send_message(call.message.chat.id,
+                                         "X-fit Studio\n\nОписание акции: Абонемент на год - 10000 руб (-30%)\n"
+                                         "на полгода - 7500 руб (-17%)\nна месяц - 3000 руб (-40%)\n\n"
+                                         "Как получить: Показать профсоюзный билет")
+                    elif call.data == 'boltay':
+                        bot.send_message(call.message.chat.id,
+                                         "кафе Boltay\n\nОписание акции: Скидка 20% на все горячие "
+                                         "напитки и воки для членов профсоюза в «счастливые часы». "
+                                         "А именно с 8:30 до 12:00 и с 19:00 до 00:00\n\n"
+                                         "Как получить: Показать профсоюзный билет")
+                    elif call.data == 'theory':
+                        bot.send_message(call.message.chat.id,
+                                         "кафе Теория\n\nОписание акции: Скидка 10% на всё для членов профсоюза.\n\n"
+                                         "Как получить: Показать профсоюзный билет")
+                    elif call.data == 'schnitzel':
+                        bot.send_message(call.message.chat.id,
+                                         "Шницельная\n\nОписание акции: Скидка 20% на всё для членов профсоюза.\n\n"
+                                         "Как получить: Показать профсоюзный билет")
+                    elif call.data == 'herb-store':
+                        bot.send_message(call.message.chat.id,
+                                         "Магазин Herb-store.ru\n\nОписание акции: "
+                                         "Скидка 10% на всё для членов профсоюза.\n\n"
+                                         "Как получить: Показать профсоюзный билет")
+                    elif call.data == 'driving school':
+                        bot.send_message(call.message.chat.id,
+                                         "Автошкола\n\nОписание акции: Обучение для студентов МФТИ, "
+                                         "заполнивших данную форму на категорию B стоит 30.000 при предъявлении "
+                                         "студенческого билета, а на категорию A стоит 15.000 (возможна рассрочка), "
+                                         "и они получат специальный подарок от автошколы. Занятия по теории одинаковы "
+                                         "для обеих категорий. Вождение у мото групп будет проходить "
+                                         "на площадке в Мытищах, туда надо будет добраться самостоятельно.\n\n"
+                                         "Как получить: https://forms.gle/QYqWQpPJ46FL5bQWA")
+                    elif call.data == 'buffet':
+                        bot.send_message(call.message.chat.id,
+                                         "Буфет тройки\n\nОписание акции: Скидка 10% на всё для членов профсоюза.\n\n"
+                                         "Как получить: Показать профсоюзный билет")
+        except Exception as e:
+            print(repr(e))
+
+    # run
+    bot.polling(none_stop=True)
+
+
+if __name__ == "__main__":
+    main()
